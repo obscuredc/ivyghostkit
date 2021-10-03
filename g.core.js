db6706c7c6b0041a1f953ab9e89cb5fe2123fc06
obscuredc <77344064+obscuredc@users.noreply.github.com>
2021-10-02 21:06:38 -0400

    Add files via upload

---
+++ g.core.js
@@ -0,0 +1,169 @@
+_SYSTEMVERSION = "ivy ghostkit 100";
+_SYSTEMDIST = "standard";
+_AUTHORS = ["obscuredc"];
+
+/** IO SECTION + UI */
+var IO = {
+    out: document.getElementById("IO-out")
+}
+
+var Message = {
+    raw: function (_msg) {
+        IO.out.innerHTML += _msg;
+    },
+    _LOCALID: 0
+}
+_SYSMESSAGE = true;
+Message.send = function (_msg, auth) {
+    Message.raw(`<p id="MessageSend${Message._LOCALID}" class="pn">${auth}:${_msg}</p>`);
+    ++Message._LOCALID;
+    return Message._LOCALID - 1;
+}
+Message.sendSystem = function (_msg, cssatr) {
+    if(_SYSMESSAGE == true) {
+        Message.raw(`<p id="MessageSend${Message._LOCALID}" class="pn t ${cssatr}"><span class="t special">${_SYSTEMAPPLICATION}</span>${_SYSTEMPATH == "" ? "" : "@" + _SYSTEMPATH}:${_msg}</p>`);
+        ++Message._LOCALID;
+        return Message._LOCALID - 1;
+    }
+}
+Message.out = {};
+Message.out.normal = function (_msg) {
+    return Message.sendSystem(_msg, "normal");
+}
+Message.out.warn = function (_msg) {
+    return Message.sendSystem(_msg, "warn");
+}
+Message.out.error = function (_msg) {
+    return Message.sendSystem(_msg, "error");
+}
+Message.out.nom = function(_msg, cssattr) {
+    Message.raw(`<p id="MessageSend${Message._LOCALID}" class="pn t ${cssattr}">${_msg}</p>`);
+    ++Message._LOCALID;
+    return Message._LOCALID - 1;
+}
+/** VirtualFile and Path */
+class file {
+    constructor(id, content, type) {
+        this.id=id;
+        this.content=content;
+        this.type=type;
+    }
+    editContent(n) {
+        this.content=n;
+    }
+}
+function _FILESYSTEMADD(file) {
+    FileSystem[file.id] = file;
+    FileSystem.discoverProps.push(file.id);
+}
+var FileSystem = {
+    discoverProps: []
+}
+var _SYSTEMPATH, _SYSTEMAPPLICATION = "";
+function _FILEFROMPATH() {
+    return _SYSTEMPATH == "" && FileSystem[_SYSTEMPATH] != undefined ? false : FileSystem[_SYSTEMPATH];
+}
+function _SYSTEMATTRCHANGE(path, app) {
+    _SYSTEMPATH = path;
+    _SYSTEMAPPLICATION = app;
+    _UPDATEKEYBOARD();
+}
+/** VoidKeyboard */
+_TYPINGINPUT = true;
+_TYPINGEDITCHAR = `\u2588`; //use unicode because it breaks when using raw character for some technical reason
+_TYPINGCURRENT = "";
+_VOIDKEYBOARD = document.getElementById("VoidKeyboard");
+function _UPDATEKEYBOARD() {
+    _VOIDKEYBOARD.innerHTML = `<span class="t special">${_SYSTEMAPPLICATION}</span>${_SYSTEMPATH == "" ? "" : "@" + _SYSTEMPATH}:` + _TYPINGCURRENT + `<span class="t blink">${_TYPINGEDITCHAR}</span>`;
+}
+document.addEventListener("keydown", function (event) {
+    switch(event.key) {
+        case "Control":
+        case "Shift":
+        case "Alt":
+        case "Escape":
+        case "Tab":
+        case "CapsLock":
+        case "Meta":
+        case "ArrowLeft":
+        case "ArrowRight":
+        case "ArrowUp":
+        case "ArrowDown":
+            break;
+        case "Backspace":
+            _TYPINGCURRENT = _TYPINGCURRENT.substring(0, _TYPINGCURRENT.length - 1);
+            _UPDATEKEYBOARD();
+            break;
+        case "Enter":
+            _EXECUTECOMMAND(_TYPINGCURRENT);
+            _TYPINGCURRENT = "";
+            _UPDATEKEYBOARD();
+            break;
+        default:
+            if(_TYPINGINPUT) {
+                _TYPINGCURRENT += event.key;
+                _UPDATEKEYBOARD();
+                break;
+            }
+    }
+})
+
+/** Command Executor */
+class _COMMAND {
+    constructor(id, onexec) {
+        this.id=id;
+        /** accept single param: it will have an ordered array by
+         * user input order of parameters. IE: `add*1,2` -> {cmd:"add",params:["1", "2"]}
+         */
+        this.onexec=onexec;
+    }
+    execute(p) {
+        this.onexec(p);
+    }
+}
+var CommandRegistry = [];
+function _COMMANDBYID(id) {
+    for(i = 0; i < CommandRegistry.length; i++) {
+        if(CommandRegistry[i].id==id) {
+            return CommandRegistry[i];
+        }
+    }
+    return false;
+}
+function _ADDCOMMAND(c) {
+    CommandRegistry.push(c);
+}
+function _PARSECOMMAND(_STRRAW) {
+    /** expect: COMMAND*PARAM1,PARAM2... */
+    var s=_STRRAW.split("*");
+    if(s[1]) {
+        return {
+            cmd:s[0],
+            params:s[1].split(",")
+        }
+    } else {
+        return {
+            cmd:s[0],
+            params:[]
+        }
+    }
+}
+function _EXECUTECOMMAND(raw) {
+    var f = _PARSECOMMAND(raw);
+    console.log(f);
+    if(_COMMANDBYID(f.cmd) != false || undefined) {
+        _COMMANDBYID(f.cmd).execute(f.params);
+    } else {
+        // Command doesn't exist!
+        Message.sendSystem(_TYPINGCURRENT);
+        Message.out.nom(`invalid cmd \`${f.cmd}\``, "error");
+    }
+}
+
+//onstart
+_SYSTEMPATH = "";
+_SYSTEMAPPLICATION = "native";
+//render block for typing
+_UPDATEKEYBOARD();
+
+Message.sendSystem("welcome to the ivy ghostkit!");
---
+++ g.std.js
@@ -0,0 +1,92 @@
+_ADDCOMMAND(new _COMMAND("cd", function(p) {
+    var c=p[0];
+    if(c == "" || c == undefined) {
+        Message.sendSystem(_TYPINGCURRENT);
+        _SYSTEMATTRCHANGE("", _SYSTEMAPPLICATION);
+    } else if(FileSystem[c]) {
+        Message.sendSystem(_TYPINGCURRENT);
+        _SYSTEMATTRCHANGE(c, _SYSTEMAPPLICATION);
+    } else {
+        //error, not a real file
+        Message.sendSystem(_TYPINGCURRENT);
+        Message.out.nom(`invalid path ${c}`, "error");
+    }
+}));
+_ADDCOMMAND(new _COMMAND("cfile", function(p) {
+    Message.sendSystem(_TYPINGCURRENT);
+    Message.out.nom(`created file ${p[0]}.${p[1]}`)
+    _FILESYSTEMADD(new file(p[0], "", p[1]));
+}))
+_ADDCOMMAND(new _COMMAND("clr", function(p) {
+    IO.out.innerHTML = "";
+}))
+_ADDCOMMAND(new _COMMAND("rfile", function (p) {
+    Message.sendSystem(_TYPINGCURRENT);
+    try {
+        Message.out.nom(FileSystem[_SYSTEMPATH].content, "normal");
+    } catch {
+        Message.out.error(`invalid file!`);
+    }
+}))
+_ADDCOMMAND(new _COMMAND("del", function(p) {
+    Message.sendSystem(_TYPINGCURRENT);
+    try {
+        if(!_SYSTEMPATH == "") {
+            FileSystem[_SYSTEMPATH] = undefined;
+            Message.out.nom("deleted current file!", "normal");
+        } else {
+            Message.out.error("cannot delete empty file!");
+        }
+    } catch {
+        Message.out.error(`invalid file!`);
+    }
+}))
+_ADDCOMMAND(new _COMMAND("retype", function(p) {
+    Message.sendSystem(_TYPINGCURRENT);
+    try {
+        if(!_SYSTEMPATH == "") {
+            FileSystem[_SYSTEMPATH].type = p[0];
+            Message.out.nom("retyped current file", "normal");
+        } else {
+            Message.out.error("cannot retype empty file!");
+        }
+    } catch {
+        Message.out.error(`invalid file!`);
+    }
+}))
+_ADDCOMMAND(new _COMMAND("info", function(p) {
+    Message.sendSystem(_TYPINGCURRENT);
+    try {
+        if(!_SYSTEMPATH == "" && _FILEFROMPATH() != false) {
+            Message.out.nom(`Type: ${_FILEFROMPATH().type},<br>
+            Name: ${_FILEFROMPATH().id}
+            `, "normal");
+        } else {
+            Message.out.error("cannot get info of empty file!");
+        }
+    } catch {
+        Message.out.error(`invalid file!`);
+    }
+}))
+_ADDCOMMAND(new _COMMAND("out", function(p) {
+    Message.sendSystem(_TYPINGCURRENT);
+    Message.out.nom(p.join(""));
+}))
+_ADDCOMMAND(new _COMMAND("version", function(p) {
+    Message.sendSystem(_TYPINGCURRENT);
+    Message.out.nom(_SYSTEMVERSION + " written by " + _AUTHORS.join(","));
+    Message.out.nom(`distribution: ${_SYSTEMDIST}.`);
+}))
+_ADDCOMMAND(new _COMMAND("help", function(p) {
+    Message.sendSystem(_TYPINGCURRENT);
+    Message.out.nom("Avalible commands:");
+    for(i=0; i<CommandRegistry.length;i++) {
+        Message.out.nom(CommandRegistry[i].id, "normal");
+    }
+}))
+_ADDCOMMAND(new _COMMAND("time", function (p) {
+    //get time
+    var d = new Date();
+    Message.sendSystem(_TYPINGCURRENT);
+    Message.out.nom(`${d.getMonth() + 1}/${d.getDate()}/${d.getFullYear()}@${d.getHours() < 24 ? (d.getHours() - 12) + "PM" : d.getHours() + "AM" }:${d.getMinutes()}.${d.getSeconds()}`);
+}))
